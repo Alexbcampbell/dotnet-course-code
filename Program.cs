c3442f0cab33ff5837275436ad33597b3244d168
Alex Campbell <alexbe.campbell@gmail.com>
2025-01-14 21:26:14 -0600

    Using old console template

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello C# Programmers");
+// Console.WriteLine("Hello C# Programmers");
+// Console.Write("First");
+// Console.Write("Second");
+// Console.WriteLine("Hello C# Programmers");
+// Console.Write("Third");
+
+// Console.WriteLine(args[0]);
+
+using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
